33cf5101e0247bd6fdbd6af09dfb18f44a1a02f8
Adler Medrado <adlermedrado@gmail.com>
2024-10-13 14:54:17 -0300

    Add symmetric encrypt and decrypt support

--- shadow_data/exceptions.py
+++ shadow_data/exceptions.py
@@ -1,4 +1,18 @@
-class InvalidEmailError(Exception):
-    def __init__(self, message='Invalid email format'):
-        self.message = message
-        super().__init__(self.message)
+class CustomError(Exception):
+    def __init__(self, message: str) -> None:
+        super().__init__(message)
+
+
+class InvalidEmailError(CustomError):
+    def __init__(self) -> None:
+        super().__init__('Invalid email format')
+
+
+class CipherKeyNotFoundError(CustomError):
+    def __init__(self) -> None:
+        super().__init__('Cipher key not found. Please create or set a key before encryption/decryption.')
+
+
+class InvalidCipherKeyError(CustomError):
+    def __init__(self) -> None:
+        super().__init__('Invalid cipher key')
